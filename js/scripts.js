6d7f2cb8415fc12d5a51bbad7060ded5d13091ed
Pshiau <116909626+Pshiau@users.noreply.github.com>
2023-02-15 23:26:31 +0100

    initial setup

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList=[
+    {name:"Bulbasaur",types:[grass,poison],height:0.7,weight:6.9},
+    {name:"Charmander",types:[fire],height:0.6,weight:8.5},
+    {name:"Squirtle",types:[water],height:0.5,weight:9},
+    {name:"Ivysaur",types:[grass,poison],height:1,weight:13}
+];
