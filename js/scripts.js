017538ab498f5731785fec8c8c1dbe91d9e912fb
Pshiau <116909626+Pshiau@users.noreply.github.com>
2023-02-16 11:41:08 +0100

    add units for weight &height of Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList=[
-    {name:"Bulbasaur",types:[grass,poison],height:0.7,weight:6.9},
-    {name:"Charmander",types:[fire],height:0.6,weight:8.5},
-    {name:"Squirtle",types:[water],height:0.5,weight:9},
-    {name:"Ivysaur",types:[grass,poison],height:1,weight:13}
+    {name:"Bulbasaur",types:[grass,poison],height:0.7+"m",weight:6.9+"kg"},
+    {name:"Charmander",types:[fire],height:0.6+"m",weight:8.5+"kg"},
+    {name:"Squirtle",types:[water],height:0.5+"m",weight:9+"kg"},
+    {name:"Ivysaur",types:[grass,poison],height:1+"m",weight:13+"kg"}
 ];
