4ca2826338917ad246f0e8da5ec0aca04707f7bc
Pshiau <116909626+Pshiau@users.noreply.github.com>
2023-02-28 23:09:48 +0100

    wrap pokemonList in an IIFE & create Add()&getAll()

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
+let pokemonRepository = (function () {
+    let pokemonList= [
+        {name:"Bulbasaur",types:["grass","poison"],height:0.7,weight:6.9},
+        {name:"Charmander",types:["fire"],height:0.6,weight:8.5},
+        {name:"Squirtle",types:["water"],height:0.5,weight:9},
+        {name:"Ivysaur",types:["grass","poison"],height:1,weight:13},
+    ];
 
-let pokemonList=[
-    {name:"Bulbasaur",types:["grass","poison"],height:0.7,weight:6.9},
-    {name:"Charmander",types:["fire"],height:0.6,weight:8.5},
-    {name:"Squirtle",types:["water"],height:0.5,weight:9},
-    {name:"Ivysaur",types:["grass","poison"],height:1,weight:13},
-    {name:"Charmeleon",type:["fire"],height:1.1,weight:19},
-];
-pokemonList.forEach(function(pokemon){
-    document.write(pokemon.name+"<br>");
-});
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+
+    function getAll(){
+        return pokemonList;
+    }
+    //using the function keyword, then only return the key-value pairs 
+   
+    return {
+        add: add,
+        getAll:getAll
+    };
+})();
+
+//using add() to add a new pokemon to the List 
+pokemonRepository.add({name:"Charmeleon",type:["fire"],height:1.1,weight:19});
 
 
-
-
-
-
+//calling pokemonList from getAll() 
+//and then use forEach() to iterate over the name & height of Pokemon from list
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + " (height: " + pokemon.height + "m)</br>"); 
+    });
 
 
